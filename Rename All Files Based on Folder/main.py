78b997dcf449df697089cd7afc1ce0495904b358
Jeffe <61993619+jenumerables@users.noreply.github.com>
2023-01-26 23:34:19 +0000

    Add rename all files based on folder

---
+++ Rename All Files Based on Folder/main.py
@@ -0,0 +1,14 @@
+# Rename All Files Based on Folder
+
+from pathlib import Path
+
+root_dir = Path('files')
+file_paths = root_dir.glob("**/*")
+
+for path in file_paths:
+  if path.is_file():
+    parent_folder = path.parts[1]
+    new_filename = parent_folder + '-' + path.name
+    print(new_filename)
+    new_filepath = path.with_name(new_filename)
+    path.rename(new_filepath)
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # Intro to Python Path Lib Library
 # Add Prefix to All Filenames in Folder
-# Next - Rename All Files Based on Folder
+# Rename All Files Based on Folder
